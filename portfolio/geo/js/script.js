932c13369a377e7301d16de38417e90c3e21cf6b
Ivan Popal <maincorp228@gmail.com>
2016-10-31 00:42:05 +0300

    geo up-date

--- portfolio/geo/js/script.js
+++ portfolio/geo/js/script.js
@@ -1,10 +1,15 @@
 'use strict';
 
 var btnSendCoords = document.querySelector('.send-coords');
+var latitude = document.querySelector('.info__latitude');
+var longitude = document.querySelector('.info__longitude');
 
 btnSendCoords.addEventListener('click', function(evt) {
   evt.preventDefault();
-  navigator.geolocation.getCurrentPosition(function(geo) {
-    console.log(geo);
+  navigator.geolocation.getCurrentPosition(function(pos) {
+    var geo = pos.coords;
+    latitude.innerHTML = geo.latitude;
+    longitude.innerTML = geo.longitude;
   });
+
 });
